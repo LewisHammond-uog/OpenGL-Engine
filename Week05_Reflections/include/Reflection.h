c84e461872bb638091ff9a904d6aca6f25e4d3ef
Lewis <s1800707@connect.glos.ac.uk>
2021-10-22 17:00:44 +0100

    Start - Pink Dragon
    Pink Dragon is rendering!

---
+++ Week05_Reflections/include/Reflection.h
@@ -0,0 +1,43 @@
+#ifndef __Shadows_H_
+#define __Shadows_H_
+
+#include "Application.h"
+#include <glm/glm.hpp>
+#include "FBXFile.h"
+
+// Derived application class that wraps up all globals neatly
+class Reflection : public Application
+{
+public:
+
+	Reflection();
+	virtual ~Reflection();
+
+protected:
+
+	virtual bool onCreate();
+	virtual void Update(float a_deltaTime);
+	virtual void Draw();
+	virtual void Destroy();
+
+	
+	glm::mat4	m_cameraMatrix;
+	glm::mat4	m_projectionMatrix;
+	glm::mat4	m_modelMatrix;
+
+	unsigned int	m_programID;
+	unsigned int	m_vertexShader;
+	unsigned int	m_fragmentShader;
+
+	unsigned int m_vao;
+	unsigned int m_vbo;
+	unsigned int m_ibo;
+
+
+	//FBX Model File
+	FBXFile* m_fbxModel;
+
+	glm::vec4 m_lightPos;
+};
+
+#endif // __Application_H_
